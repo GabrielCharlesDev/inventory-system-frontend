89187b4d8e31f7dda424e534f6138a1efa240ccb
GabrielCharlesDev <gabrielcharlesmz@gmail.com>
2025-06-24 11:10:26 -0600

    Add frontend project selectively

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ProductoLista } from "./producto-lista/producto-lista";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+ 
 })
 export class App {
   protected title = 'inventario-app';
---
+++ src/app/producto.ts
@@ -0,0 +1,7 @@
+export class Producto {
+    idProducto: number;
+    descripcion!: string; // != se indica que se inicializa mas tarde
+    precio: number;
+    existencia: number;
+    
+}
